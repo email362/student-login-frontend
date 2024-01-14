f4a720940dd7c9a67d703fd4b8e35212f0289c2f
Chris <hyer.chris@gmail.com>
2024-01-14 15:27:04 -0800

    Update vite.config.js
    removed custom base directory

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/student-login-frontend/', // Base url for GH Pages https://dev.to/shashannkbawa/deploying-vite-app-to-github-pages-3ane
   plugins: [react()],
 })
