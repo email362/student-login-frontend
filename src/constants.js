99972b7cde307693d64c47a2318edc6b8a5baf11
Chris <hyer.chris@gmail.com>
2024-05-27 19:10:32 -0700

    Update constants.js
    updated backend url to updated host site

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 const PORT = 5100;
-const prodURL = 'https://vivacious-jade-nightgown.cyclic.app';
+const prodURL = 'https://student-login-app-9wnk.onrender.com';
 const devURL = `http://localhost:${PORT}`;
 export const URL = import.meta.env.MODE == 'development' ? devURL : prodURL;
